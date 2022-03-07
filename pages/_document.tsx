f9cac4ea58c41456ec20cac152620bdf46362006
Mahim Safa <mahimsafa@gmail.com>
2022-03-07 18:41:58 +0600

    added google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-
+import Script from "next/script";
 
 class MyDocument extends Document {
   render() {
@@ -12,6 +12,17 @@ class MyDocument extends Document {
             rel="stylesheet"
           />
         </Head>
+        <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'G-6PGQJC0FJ1', 'auto');
+          ga('send', 'pageview');
+        `}
+      </Script>
         {/* <body className="bg-fixed bg-gradient-to-r from-green to-blue-500 dark:from-dark-500 dark:to-dark-700 dark:text-white"> */}
         <body className="bg-fixed  bg-gradient-to-r from-green-400 to-green-400 dark:from-dark-500 dark:to-dark-700 dark:text-white">
             <Main />
