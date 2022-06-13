c990aaa659cf86a6e5576f5dca8618b03d592744
Mahim Safa <mahimsafa@gmail.com>
2022-06-13 14:38:55 +0600

    updated img source domain list

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   images: {
-    domains: ['raw.githubusercontent.com'],
+    domains: ['raw.githubusercontent.com', 'camo.githubusercontent.com', '*.githubusercontent.com', 'i.ibb.co'],
   },
 };
